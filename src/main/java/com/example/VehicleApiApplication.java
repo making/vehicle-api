4e333d2c8a17d287b0ceb3fba765445391dffd14
Toshiaki Maki <tmaki@vmware.com>
2021-06-25 20:56:29 +0900

    Use AotProxyHint

--- src/main/java/com/example/VehicleApiApplication.java
+++ src/main/java/com/example/VehicleApiApplication.java
@@ -2,8 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
 
 @SpringBootApplication
+@AotProxyHint(targetClass = com.example.VehicleMapper.class, proxyFeatures = ProxyBits.IS_STATIC)
 public class VehicleApiApplication {
 
 	public static void main(String[] args) {
