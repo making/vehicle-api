94c8262ad3e4c57934523e8e52df0168f06d359c
Toshiaki Maki <tmaki@vmware.com>
2023-04-06 12:07:32 +0900

    Update config

--- src/main/java/com/example/AppConfig.java
+++ src/main/java/com/example/AppConfig.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import am.ik.accesslogger.AccessLogger;
+import io.micrometer.core.instrument.config.MeterFilter;
 import org.mybatis.scripting.thymeleaf.SqlGenerator;
 import org.mybatis.scripting.thymeleaf.SqlGeneratorConfig;
 
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +18,24 @@ public class AppConfig {
 		final SqlGeneratorConfig config = SqlGeneratorConfig.newInstanceWithCustomizer(c ->
 				c.getDialect().setBindVariableRenderInstance(SPRING_NAMED_PARAMETER));
 		return new SqlGenerator(config);
+	}
+
+	@Bean
+	public AccessLogger accessLogger() {
+		return new AccessLogger(httpExchange -> {
+			final String uri = httpExchange.getRequest().getUri().getPath();
+			return uri != null && !(uri.equals("/readyz") || uri.equals("/livez")
+									|| uri.startsWith("/actuator") || uri.startsWith("/_static"));
+		});
+	}
+
+	@Bean
+	public MeterRegistryCustomizer<?> meterRegistryCustomizer() {
+		return registry -> registry.config() //
+				.meterFilter(MeterFilter.deny(id -> {
+					final String uri = id.getTag("uri");
+					return uri == null || uri.equals("/readyz") || uri.equals("/livez")
+						   || uri.startsWith("/actuator") || uri.startsWith("/_static");
+				}));
 	}
 }
