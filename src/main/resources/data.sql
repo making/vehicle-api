d58af8b556e5a683d0650433baa9c75e357ee146
Toshiaki Maki <makingx@gmail.com>
2024-01-11 16:58:29 +0900

    Update sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO vehicle(name) VALUES ('Avalon') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Corolla') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Crown') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Levin') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Yaris') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Vios') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Glanza') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
-INSERT INTO vehicle(name) VALUES ('Aygo') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Accord') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Civic') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('CR-V') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Fit') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('HR-V') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Insight') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Odyssey') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Pilot') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
